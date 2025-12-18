da30ae5e51a425333571f4f4aacdc4d182a6eb41
Abhijit RAY <royabhijits928@gmail.com>
2025-12-18 21:07:06 +0530

    Create variable.tf

---
+++ Ec2-With-IAM-Role/variable.tf
@@ -0,0 +1,35 @@
+# Terraform-Module-Project/Ec2-With-IAM-Role/variables.tf
+
+variable "aws_region" {
+       description = "The AWS region to deploy resources in"
+       type        = string
+       default     = "us-west-2"
+}
+variable "vpc_cidr" {
+       description = "The CIDR block for the VPC"
+       type        = string
+       default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+       description = "The CIDR block for the subnet"
+       type        = string
+       default     = "10.0.1.0/24"
+}
+
+variable "key_name" {
+       description = "The name of the key pair to use for the EC2 instance"
+       type        = string
+       default     = "Existing Ec2 key pair"
+  
+}
+#variable "instance_type" {
+#       description = "The type of EC2 instance to launch"
+#       type        = string
+#       default     = "t2.micro"
+#}
+#variable "ami_id" {
+#       description = "The AMI ID for the EC2 instance"
+#       type        = string
+#       default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+#}
