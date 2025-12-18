6342c52a7238c7e69e5b92d4a014e9d5a528543c
Abhijit RAY <royabhijits928@gmail.com>
2025-12-18 21:05:07 +0530

    Create main.tf

---
+++ Ec2-With-IAM-Role/modules/vpc/main.tf
@@ -0,0 +1,36 @@
+resource "aws_vpc" "main" {
+       cidr_block = var.vpc_cidr
+       tags = {
+              Name = "iam-ec2-vpc"
+       }
+
+} 
+
+resource "aws_subnet" "public" {
+       vpc_id = aws_vpc.main.id
+       cidr_block = var.subnet_cidr
+       map_public_ip_on_launch = true
+       tags = {
+              Name = "iam-ec2-public-subnet"
+       }
+} 
+
+resource "aws_internet_gateway" "igw" {
+       vpc_id = aws_vpc.main.id
+       tags = {
+              Name = "iam-ec2-igw"
+       }
+}
+
+resource "aws_route_table" "public" {
+       vpc_id = aws_vpc.main.id
+       route {
+              cidr_block = "0.0.0.0/0"
+              gateway_id = aws_internet_gateway.igw.id
+}
+}
+resource "aws_route_table_association" "master" {
+       subnet_id = aws_subnet.public.id
+       route_table_id = aws_route_table.public.id
+
+} 
